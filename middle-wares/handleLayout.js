bfacda8eb68f2ac2e6b007e7fd026651777683a6
phanthaoIT <ptthao.itus@gmail.com>
2018-05-23 13:19:34 +0700

    load menu từ database

--- middle-wares/handleLayout.js
+++ middle-wares/handleLayout.js
@@ -1,6 +1,7 @@
 var handleLayout = require('../app/Model/handleLayout');
 module.exports = (req, res, next) => {
     handleLayout.loadAll().then(rows => {
+    	console.log(rows);
         res.locals.layoutVM = {
             LoaiXe: rows[0],
             HangXe: rows[1]
