efea7e6c7c5a7d386d6c11ae7c9defa3a097aac8
Thanh <thanhvantran0@gmail.com>
2018-05-29 22:37:14 +0700

    Them controler login

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,12 @@
+var loginController = require("../app/controllers/loginController.js")
+
+
 module.exports = (app) => {
     app.get('/', (req, res) => {
         res.render('index', {
             title: 'Express'
         })
     })
+
+    // app.use("/login",loginController);
 }
