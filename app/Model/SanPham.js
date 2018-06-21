7aca1ea66b032c9f1e9619bbfe83286e6c973c10
phanthaoIT <ptthao.itus@gmail.com>
2018-06-21 23:30:36 +0700

    update từ master

--- app/Model/SanPham.js
+++
@@ -1,6 +0,0 @@
-var db = require('../../config/mysql')
-
-exports.loadAll=()=>{
-	var sql = 'select * from sanpham order by NgayNhap limit 10;select * from sanpham order by SLBan limit 10;select * from sanpham order by LuotXem limit 10';
-	return db.load(sql);
-}
--- app/Model/handleLayout.js
+++
@@ -1,6 +0,0 @@
-var db = require('../../config/mysql')
-
-exports.loadAll=()=>{
-	var sql = 'select * from loaixe; select * from hangxe';
-	return db.load(sql);
-}
