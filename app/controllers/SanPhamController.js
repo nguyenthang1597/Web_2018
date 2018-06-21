0257da1fef90e60dea90475e416d2a1775512ace
phanthaoIT <ptthao.itus@gmail.com>
2018-06-21 23:31:25 +0700

    update từ master

---
+++ app/controllers/SanPhamController.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var SP = require('../Model/SanPham.js')
+var router = express.Router();
+router.get("/",(req,res)=> {
+	SP.loadAll().then(rows => {
+		console.log(rows);
+        var vm = {
+            moinhat: rows[0],
+            phobien: rows[1],
+            xemnhieu:rows[2],
+        };
+        res.render('index', vm);
+    });
+});
+module.exports=router;
