c56678434516b9097b2e76188da03cb42f478a15
ngtritham <ngtritham@gmail.com>
2018-06-22 03:00:24 +0700

    Them xem thong tin ca nhan

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
-var loginController = require("../app/controllers/loginController.js")
-var signupController = require('../app/controllers/signupControler.js')
-var SP = require("../app/controllers/SanPhamController")
+var loginController = require("../app/controllers/loginController.js");
+var signupController = require('../app/controllers/signupControler.js');
+var SP = require("../app/controllers/SanPhamController");
+var profileController = require('../app/controllers/profileControler.js');
 var restrict = require('../middle-wares/restrict.js');
 
 module.exports = (app) => {
 	app.use('/',SP);
 	app.use("/login",loginController);
 	app.use('/signup',signupController);
+	app.use('/profile', profileController);
 }
