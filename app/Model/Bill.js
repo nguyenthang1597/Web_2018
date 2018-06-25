88fd9f0cf83d26a0e0c8e02f8e5c6032ce4952a2
Thanh <thanhvantran0@gmail.com>
2018-06-25 18:47:40 +0700

    Xong phần bill/list, bill/info

--- app/Model/Bill.js
+++ app/Model/Bill.js
@@ -2,7 +2,7 @@ const db = require('../../config/mysql');
 
 var Bill = {
     loadAllBill: () => {
-        return db(`select hoadon.MaHoaDon as Id, khachhang.Ten as TenKhachHang, chitiethoadon.TinhTrang as TinhTrang, DATE_FORMAT(chitiethoadon.NgayMua,'%d/%m/%y') as NgayMua
+        return db(`select distinct hoadon.MaHoaDon as Id, khachhang.Ten as TenKhachHang, chitiethoadon.TinhTrang as TinhTrang, DATE_FORMAT(chitiethoadon.NgayMua,'%d/%m/%y') as NgayMua
         from hoadon
             inner join chitiethoadon on hoadon.MaHoaDon = chitiethoadon.MaHoaDon
             inner join khachhang on chitiethoadon.IdKH = khachhang.Id`)
@@ -13,14 +13,19 @@ var Bill = {
     getBillById: (id) => {
         return db(`select IdSP,TenSP,SL,DonGia,XuatXu
         from hoadon
-            inner join sanpham on hoadon.IdSP = sanpham.Id`)
+            inner join sanpham on hoadon.IdSP = sanpham.Id
+        where hoadon.MaHoaDon = ${id}`)
     },
     getInfoById: (id) => {
-        return db(`select hoadon.MaHoaDon as MaHoaDon, khachhang.Ten as TenKhachHang,
+        return db(`select distinct hoadon.MaHoaDon as MaHoaDon, khachhang.Ten as TenKhachHang,
         IdKH,DiaChi,SDT,Email,TongTien,TinhTrang
         from hoadon
             inner join chitiethoadon on chitiethoadon.MaHoaDon = hoadon.MaHoaDon
-            inner join khachhang on IdKH = khachhang.Id`)
+            inner join khachhang on IdKH = khachhang.Id
+        where hoadon.MaHoaDon = ${id}`)
+    },
+    updateTinhTrang: (id,tinhtrang) =>{
+        return db(`update chitiethoadon set TinhTrang = '${tinhtrang}' where MaHoaDon = '${id}'`)
     }
 }
 
