04c5f1c1a38b400e730fa4bc6fb672c9f3d10e29
ngtritham <ngtritham@gmail.com>
2018-06-27 21:04:51 +0700

    update

--- app/controllers/cartController.js
+++ app/controllers/cartController.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const account = require('../Model/cartModel.js')
+const cartModel = require('../Model/cartModel.js');
+const mw = require('../../middle-wares/middlewares');
+const accountModel = require('../Model/Account');
+var db = require('../../config/mysql');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('cart/index');
+router.get('/', mw.isLoggedInUser, (req, res) => {
+    res.render('user/cart');
 });
 
 module.exports = router;
