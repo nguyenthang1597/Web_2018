20ff9c2584bb7384c6ae352a81ab96275768c670
Thanh <thanhvantran0@gmail.com>
2018-06-28 11:26:54 +0700

    Thêm phần cập nhật  thông tin cá nhân khách hàng

--- app/Model/Account.js
+++ app/Model/Account.js
@@ -1,4 +1,5 @@
 const db = require('../../config/mysql');
+const moment = require('moment')
 
 module.exports = {
     loadAllUserAccount: () => {
@@ -15,5 +16,8 @@ module.exports = {
     },
     loadProfileUserById: (id) => {
         return db(`select * from khachhang where id = ${id}`);
+    },
+    updateProfileUser: (data) => {
+        return db(`update khachhang set Ten = '${data.Ten}',Email = '${data.Email}',SDT ='${data.SDT}',NgaySinh = '${moment(data.NgaySinh).format('YYYY/MM/DD')}',DiaChi = '${data.DiaChi}',CMND = '${data.CMND}' where Id = '${data.id}'`)
     }
 }
