668a34e3b512bfa1208ef157ce4b8cf7bf219bc1
Thanh <thanhvantran0@gmail.com>
2018-06-28 22:23:53 +0700

    Cập nhật add id vào bảng khách hàng khi đăng kí

--- config/mysql.js
+++ config/mysql.js
@@ -5,7 +5,7 @@ module.exports = (sql) => {
 		const cn = mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
-			password: 'Hieu19091997',
+			password: '',
 			database: 'banoto',
 			dateStrings: 'date',
 			multipleStatements: true
